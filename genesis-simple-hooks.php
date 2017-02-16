be47a08df415cee5582012df10d637c2a5c564dd
Nathan Rice <ncrice@gmail.com>
2017-02-16 15:58:16 -0500

    Prevent 2 entries in plugin list.

--- genesis-simple-hooks.php
+++ genesis-simple-hooks.php
@@ -1,21 +1,4 @@
 <?php
-/*
-Plugin Name: Genesis Simple Hooks
-Plugin URI: http://www.studiopress.com/plugins/simple-hooks
-
-Description: Genesis Simple Hooks allows you easy access to the 50+ Action Hooks in the Genesis Theme.
-
-Author: Nathan Rice
-Author URI: http://www.nathanrice.net/
-
-Version: 2.2.0
-
-Text Domain: genesis-simple-hooks
-Domain Path: /languages
-
-License: GNU General Public License v2.0 (or later)
-License URI: http://www.opensource.org/licenses/gpl-license.php
-*/
 
 class Genesis_Simple_Hooks {
 
--- plugin.php
+++ plugin.php
@@ -1,13 +1,20 @@
 <?php
 /*
 Plugin Name: Genesis Simple Hooks
+Plugin URI: http://www.studiopress.com/plugins/simple-hooks
+
+Description: Genesis Simple Hooks allows you easy access to the 50+ Action Hooks in the Genesis Theme.
+
+Author: Nathan Rice
+Author URI: http://www.nathanrice.net/
+
+Version: 2.2.0
+
+Text Domain: genesis-simple-hooks
+Domain Path: /languages
+
+License: GNU General Public License v2.0 (or later)
+License URI: http://www.opensource.org/licenses/gpl-license.php
 */
 
-add_action( 'admin_init', 'genesis_simple_hooks_switch_plugin_file' );
-/**
- * Make WordPress switch from using this file, to using the main plugin file.
- */
-function genesis_simple_hooks_switch_plugin_file() {
-	deactivate_plugins( plugin_basename( __FILE__ ) );
-	activate_plugin( plugin_dir_path( __FILE__ ) . 'genesis-simple-hooks.php' );
-}
+require_once( plugin_dir_path( __FILE__ ) . 'genesis-simple-hooks.php' );
