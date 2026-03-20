c58c4e510f209b2b0b5c3173b4b211c7067a6e81
Nick Cernis <nick@cern.is>
2026-03-20 18:13:51 +0100

    fix: remove duplicate header
    Fixes #28.

--- includes/class-genesis-simple-hooks.php
+++ includes/class-genesis-simple-hooks.php
@@ -6,21 +6,7 @@
  */
 
 /**
- * Plugin Name: Genesis Simple Hooks
- * Plugin URI: http://www.studiopress.com/plugins/simple-hooks
- *
- * Description: Genesis Simple Hooks allows you easy access to the 50+ Action Hooks in the Genesis Theme.
- *
- * Author: StudioPress
- * Author URI: http://www.studiopress.com/
- *
- * Version: 2.3.1
- *
- * Text Domain: genesis-simple-hooks
- * Domain Path: /languages
- *
- * License: GNU General Public License v2.0 (or later)
- * License URI: http://www.opensource.org/licenses/gpl-license.php
+ * Genesis Simple Hooks allows easy access to the 50+ Action Hooks in the Genesis Theme.
  */
 class Genesis_Simple_Hooks {
 
